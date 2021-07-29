e07c983fb7e36a6311fcaea803382f1ba3b48bbc
Lynn Stephenson <63118982+lynn-stephenson@users.noreply.github.com>
2021-07-28 21:08:09 -0800

    Add test vectors for row_round function

--- src/lib.rs
+++ src/lib.rs
@@ -103,4 +103,47 @@ mod tests {
             [0x3e2f308c, 0xd90a8f36, 0x6ab2a923, 0x2883524c]
         );
     }
+
+    #[test]
+    fn row_round_test_vector_1() {
+        let mut state = [
+            0x00000001, 0x00000000, 0x00000000, 0x00000000,
+            0x00000001, 0x00000000, 0x00000000, 0x00000000,
+            0x00000001, 0x00000000, 0x00000000, 0x00000000,
+            0x00000001, 0x00000000, 0x00000000, 0x00000000
+        ];
+
+        row_round(&mut state);
+
+        assert_eq!(state,
+            [
+                0x08008145, 0x00000080, 0x00010200, 0x20500000,
+                0x20100001, 0x00048044, 0x00000080, 0x00010000,
+                0x00000001, 0x00002000, 0x80040000, 0x00000000,
+                0x00000001, 0x00000200, 0x00402000, 0x88000100
+            ]
+        );
+    }
+
+    #[test]
+    fn row_round_test_vector_2() {
+        let mut state = [
+            0x08521bd6, 0x1fe88837, 0xbb2aa576, 0x3aa26365,
+            0xc54c6a5b, 0x2fc74c2f, 0x6dd39cc3, 0xda0a64f6,
+            0x90a2f23d, 0x067f95a6, 0x06b35f61, 0x41e4732e,
+            0xe859c100, 0xea4d84b7, 0x0f619bff, 0xbc6e965a
+        ];
+
+        row_round(&mut state);
+
+        assert_eq!(
+            state,
+            [
+                0xa890d39d, 0x65d71596, 0xe9487daa, 0xc8ca6a86,
+                0x949d2192, 0x764b7754, 0xe408d9b9, 0x7a41b4d1,
+                0x3402e183, 0x3c3af432, 0x50669f96, 0xd89ef0a8,
+                0x0040ede5, 0xb545fbce, 0xd257ed4f, 0x1818882d
+            ]
+        );
+    }
 }
