ddbb4b5f32a798aeb29da5334b7f8a7d1da299f2
Lynn Stephenson <63118982+lynn-stephenson@users.noreply.github.com>
2021-07-31 02:35:58 -0800

    Fix unit tests and remove uneeded code

--- src/lib.rs
+++ src/lib.rs
@@ -124,13 +124,11 @@ fn apply_key_stream16(
     buffer: &mut [u8]
 ) {
     for (position, buffer_chunk) in buffer.chunks_mut(64).enumerate() {
-        let mut key_stream = expand16(
+        let key_stream = expand16(
             key,
             number_used_once,
             (position as u64).to_ne_bytes()
         );
-
-        hash(&mut key_stream);
 
         for index in 0..buffer_chunk.len() {
             buffer_chunk[index] ^= key_stream[index];
@@ -144,13 +142,11 @@ fn apply_key_stream32(
     buffer: &mut [u8]
 ) {
     for (position, buffer_chunk) in buffer.chunks_mut(64).enumerate() {
-        let mut key_stream = expand32(
+        let key_stream = expand32(
             key,
             number_used_once,
             (position as u64).to_ne_bytes()
         );
-
-        hash(&mut key_stream);
 
         for index in 0..buffer_chunk.len() {
             buffer_chunk[index] ^= key_stream[index];
@@ -464,34 +460,13 @@ mod tests {
     }
 
     #[test]
-    fn expand16_test_vector_1() {
+    fn expand16_test_vector() {
         assert_eq!(
             expand16(
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
                 [101, 102, 103, 104, 105, 106, 107, 108],
                 [109, 110, 111, 112, 113, 114, 115, 116]
             ),
-            [
-                101,120,112, 97, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-                13, 14, 15, 16,110,100, 32, 49,101,102,103,104,105,106,107,108,
-                109,110,111,112,113,114,115,116, 54, 45, 98,121, 1, 2, 3, 4,
-                5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,116,101, 32,107
-            ]
-        );
-    }
-
-    #[test]
-    fn expand16_test_vector_2() {
-        let mut key_stream = expand16(
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
-            [101, 102, 103, 104, 105, 106, 107, 108],
-            [109, 110, 111, 112, 113, 114, 115, 116]
-        );
-
-        hash(&mut key_stream);
-
-        assert_eq!(
-            key_stream,
             [
                 39,173, 46,248, 30,200, 82, 17, 48, 67,254,239, 37, 18, 13,247,
                 241,200, 61,144, 10, 55, 50,185, 6, 47,246,253,143, 86,187,225,
@@ -502,7 +477,7 @@ mod tests {
     }
 
     #[test]
-    fn expand32_test_vector_1() {
+    fn expand32_test_vector() {
         assert_eq!(
             expand32(
                 [
@@ -514,31 +489,6 @@ mod tests {
                 [109, 110, 111, 112, 113, 114, 115, 116]
             ),
             [
-                101,120,112, 97, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-                13, 14, 15, 16,110,100, 32, 51,101,102,103,104,105,106,107,108,
-                109,110,111,112,113,114,115,116, 50, 45, 98,121,201,202,203,204,
-                205,206,207,208,209,210,211,212,213,214,215,216,116,101, 32,107
-            ]
-        );
-    }
-
-    #[test]
-    fn expand32_test_vector_2() {
-        let mut key_stream = expand32(
-            [
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-                201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211,
-                212, 213, 214, 215, 216
-            ],
-            [101, 102, 103, 104, 105, 106, 107, 108],
-            [109, 110, 111, 112, 113, 114, 115, 116]
-        );
-
-        hash(&mut key_stream);
-
-        assert_eq!(
-            key_stream,
-            [
                 69, 37, 68, 39, 41, 15,107,193,255,139,122, 6,170,233,217, 98,
                 89,144,182,106, 21, 51,200, 65,239, 49,222, 34,215,114, 40,126,
                 104,197, 7,225,197,153, 31, 2,102, 78, 76,176, 84,245,246,184,
@@ -549,7 +499,7 @@ mod tests {
 
     #[test]
     fn apply_key_stream16_encryption_and_decryption_test() {
-        let mut buffer = "🗻∈🌏";
+        let buffer = "🗻∈🌏";
 
         for _times in 0..1 {
             apply_key_stream16(
@@ -564,7 +514,7 @@ mod tests {
 
     #[test]
     fn apply_key_stream32_encryption_and_decryption_test() {
-        let mut buffer = "🗻∈🌏";
+        let buffer = "🗻∈🌏";
 
         for _times in 0..1 {
             apply_key_stream32(
